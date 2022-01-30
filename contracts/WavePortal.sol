15aac82152b0da5c71a3937a0ed327e1f503a129
Prathamesh <prathameshirappadongritot.201cs242@nitk.edu.in>
2022-01-30 23:55:08 +0400

    Backend for decentralist

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,77 @@
+//SPDX LICENSE IDENTIFIER: UNLICENCED 
+
+pragma solidity ^0.8.4;
+
+import "hardhat/console.sol";
+
+contract WavePortal {
+    uint256 totalWaves;
+    uint private seed;
+
+     event NewWave(address indexed from, uint256 timestamp, string message);
+
+      struct Wave {
+        address waver; 
+        string message; 
+        uint256 timestamp; 
+    }
+
+     Wave[] waves;
+
+     mapping(address => uint256) public lastWavedAt;
+
+
+    constructor() payable {
+        console.log("bitcoin needs to cont(r)act 100k asap");
+
+        seed = (block.timestamp + block.difficulty) % 100;
+    }
+
+
+    function wave(string memory _message) public {
+        
+  
+    require(
+        lastWavedAt[msg.sender] + 2 minutes < block.timestamp, "Wait 2 minutes!"
+    );
+
+
+
+    lastWavedAt[msg.sender] = block.timestamp;
+
+
+
+    totalWaves += 1;
+    console.log("%s has waved!", msg.sender);
+
+    waves.push(Wave(msg.sender, _message, block.timestamp));
+
+    seed = (block.difficulty + block.timestamp + seed) % 100;
+    console.log("Random # generated: %d",seed);
+
+    if(seed <= 33) {
+        console.log("%s won!", msg.sender);
+
+        uint256 prizeAmount = 0.0001 ether;
+    require(
+        prizeAmount <= address(this).balance,
+        "Trying to withdraw more money than the contract has."
+    );
+    (bool success, ) = (msg.sender).call{value: prizeAmount}("");
+    require(success, "Failed to withdraw money from contract.");
+    }
+
+    emit NewWave(msg.sender, block.timestamp, _message);
+}
+
+     function getAllWaves() public view returns (Wave[] memory) {
+        return waves;
+    }
+
+
+    function getTotalWaves() public view returns(uint256){
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+}
+
---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+// require("@nomiclabs/hardhat-waffle");
+
+// // This is a sample Hardhat task. To learn how to create your own go to
+// // https://hardhat.org/guides/create-task.html
+// task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//   const accounts = await hre.ethers.getSigners();
+
+//   for (const account of accounts) {
+//     console.log(account.address);
+//   }
+// });
+
+// // You need to export an object to set up your config
+// // Go to https://hardhat.org/config/ to learn more
+
+// /**
+//  * @type import('hardhat/config').HardhatUserConfig
+//  */
+// module.exports = {
+//   solidity: "0.8.4",
+// };
+
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://eth-rinkeby.alchemyapi.io/v2/kOnEoZa_vyg4BvUas7fOBR-iheT4vOSC",
+      accounts: [
+        "e3ca4b513993bf734519d8fef9663debd99072ff02f40bd35f2d55547bb2ba90",
+      ],
+    },
+  },
+};
